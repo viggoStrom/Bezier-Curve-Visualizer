f24b693c9cc31aed113b5bf3a60d8f13d6e3b562
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2022-12-15 18:40:38 +0100

    linear interpolation

---
+++ script/linearInterpolationLine.js
@@ -0,0 +1,10 @@
+/** @type {HTMLCanvasElement} */
+
+
+const linearInterpolation = (u) => {
+    if (u < 0 || u > 1) {
+        return console.error(`LinearInterpolation(): u is only defined for 0 <= u <= 1 and ${u} is not within the span`);
+    }
+
+    ctx.moveTo
+}
--- script/main.js
+++ script/main.js
@@ -1,1 +1,12 @@
-/**  **/
+/** @type {HTMLCanvasElement} */
+
+
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+// defines the internal pixel width and height that differs from the css style
+canvas.width = "1000"
+canvas.height = canvas.width * 3/2
+
+
+linearInterpolation(0.5)
