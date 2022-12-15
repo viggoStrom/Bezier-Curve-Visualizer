1fdf3cb33a4c3632d0e9e7652cfd349c38aaf254
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2022-12-15 19:27:32 +0100

    more lines

--- script/linearInterpolationLine.js
+++ script/linearInterpolationLine.js
@@ -1,10 +1,51 @@
 /** @type {HTMLCanvasElement} */
 
 
-const linearInterpolation = (u) => {
-    if (u < 0 || u > 1) {
-        return console.error(`LinearInterpolation(): u is only defined for 0 <= u <= 1 and ${u} is not within the span`);
+class linearInterpolation {
+    constructor(xy1, xy2) {
+        this.x1 = xy1[0]
+        this.y1 = xy1[1]
+        this.x2 = xy2[0]
+        this.y2 = xy2[1]
+        this.uRadius = 10
+        this.straightLineModifier = (this.y2 - this.y1) / (this.x2 - this.x1)
     }
 
-    ctx.moveTo
+    drawLine = () => {
+        ctx.strokeStyle = "#252525"
+        ctx.lineWidth = 4
+
+        ctx.beginPath()
+        ctx.moveTo(this.x1, this.y1)
+        ctx.lineTo(this.x2, this.y2)
+        ctx.stroke()
+    }
+
+    setU = (u) => {
+        if (u < 0 || u > 1) {
+            return console.error(`LinearInterpolation(): u is only defined for 0 <= u <= 1 and ${u} is not within the span`);
+        }
+
+        this.ux = this.x1 + u * (this.x2 - this.x1)
+        this.uy = this.y1 + u * (this.y2 - this.y1)
+    }
+
+    getU = () => {
+        return [this.ux, this.uy]
+    }
+
+    drawU = () => {
+        ctx.strokeStyle = "#363636"
+        ctx.lineWidth = 4
+
+        ctx.beginPath()
+        ctx.arc(this.ux, this.uy, this.uRadius, 0, 2 * Math.PI)
+        ctx.stroke()
+    }
+
+    update = (u) => {
+        this.drawLine()
+        this.drawU()
+        this.setU(u)
+    }
 }
--- script/main.js
+++ script/main.js
@@ -5,8 +5,17 @@ const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 // defines the internal pixel width and height that differs from the css style
-canvas.width = "1000"
-canvas.height = canvas.width * 3/2
+canvas.width = 1000
+canvas.height = canvas.width * 2 / 3
 
+const line1 = new linearInterpolation([canvas.width * 1 / 6, 500], [canvas.width * 1 / 3], 200)
+const line2 = new linearInterpolation([canvas.width * 1 / 3, 200], [canvas.width * 2 / 3], 200)
+const line3 = new linearInterpolation([canvas.width * 2 / 3, 200], [canvas.width * 5 / 6], 500)
 
-linearInterpolation(0.5)
+// const line4 = new linearInterpolation(line1.getU, line2.getU)
+
+line1.update(.3)
+line2.update(.3)
+line3.update(.3)
+
+// line4.update(.3)
